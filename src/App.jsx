eff3e6433daa9160de1d67afaf2cb4da81c83b68
developerPCR <icrpablo.1996@gmail.com>
2023-09-30 21:57:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import AsideMenu from './components/AsideMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AsideMenu/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AsideMenu.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AsideMenu() {
+  return (
+    <div className="">
+        <h2>Guias</h2>
+        <ul>
+            <li><a href="http://">pages</a></li>
+            <li><a href="http://">pages</a></li>
+            <li><a href="http://">pages</a></li>
+            <li><a href="http://">pages</a></li>
+            <li><a href="http://">pages</a></li>
+        </ul>
+    </div>
+  );
+}
+
+export default AsideMenu;
